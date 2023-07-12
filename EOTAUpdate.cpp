eea9714014987a615e392ea17ba2c8e85f59ab20
Uri Shani <36714978+UriShX@users.noreply.github.com>
2023-07-12 16:58:30 +0300

    bug fix?

--- EOTAUpdate.cpp
+++ EOTAUpdate.cpp
@@ -26,7 +26,7 @@ _eota_reponses_t EOTAUpdate::Check(bool force)
     const bool lastCheckIsRecent = (millis() - _lastUpdateMs < _updateIntervalMs);
     if (!force && hasEverChecked && lastCheckIsRecent)
     {
-        return false;
+        return eota_no_match;
     }
 
     if (WiFi.status() != WL_CONNECTED)
